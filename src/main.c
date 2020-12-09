1b09dbbf5d0f9be55eb601bc0df162b805c2f46a
Niyamaka <niyamaka@yuneta.io>
2020-12-09 16:13:20 +0100

    yuneta version 4.2.28, all node's api has changed

--- src/main.c
+++ src/main.c
@@ -46,7 +46,7 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state);
 #define APP_NAME        "ycommand"
 #define APP_DOC         "Yuneta Command"
 
-#define APP_VERSION     "4.2.27"
+#define APP_VERSION     "4.2.28"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
